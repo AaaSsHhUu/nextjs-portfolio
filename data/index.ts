24834280d5883cee8bd9500d4bbc8c1fe7bd1583
Ashu Negi <anegi6723@gmail.com>
2025-01-24 23:54:10 +0530

    modified projects details

--- data/index.ts
+++ data/index.ts
@@ -1,10 +1,10 @@
 export const navItems = [
-    { name: "About", link: "#about" },
-    { name: "Projects", link: "#projects" },
-    { name: "Testimonials", link: "#testimonials" },
-    { name: "Contact", link: "#contact" },
-  ];
-  
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
 export const gridItems = [
   {
     id: 1,
@@ -68,153 +68,153 @@ export const gridItems = [
     spareImg: "",
   },
 ];
-  
-  export const projects = [
-    {
-      id: 1,
-      title: "3D Solar System Planets to Explore",
-      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-      img: "/p1.svg",
-      gitLink: "https://github.com/adrianhajdin?tab=repositories",
-      liveLink : "#"
-    },
-    {
-      id: 2,
-      title: "Yoom - Video Conferencing App",
-      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-      img: "/p2.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-      gitLink: "https://github.com/adrianhajdin/zoom-clone",
-      liveLink : "#"
-    },
-    {
-      id: 3,
-      title: "AI Image SaaS - Canva Application",
-      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-      img: "/p3.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-      gitLink: "https://github.com/adrianhajdin/ai_saas_app",
-      liveLink : "#"
-    },
-    {
-      id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-      img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      gitLink: "https://github.com/adrianhajdin/iphone",
-      liveLink : "#"
-    },
-  ];
-  
-  export const testimonials = [
-    {
-      quote:
-        "Collaborating with Ashu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ashu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ashu is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Ashu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ashu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ashu is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Ashu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ashu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ashu is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Ashu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ashu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ashu is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Ashu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ashu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ashu is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-  ];
-  
-  export const companies = [
-    {
-      id: 1,
-      name: "cloudinary",
-      img: "/cloud.svg",
-      nameImg: "/cloudName.svg",
-    },
-    {
-      id: 2,
-      name: "appwrite",
-      img: "/app.svg",
-      nameImg: "/appName.svg",
-    },
-    {
-      id: 3,
-      name: "HOSTINGER",
-      img: "/host.svg",
-      nameImg: "/hostName.svg",
-    },
-    {
-      id: 4,
-      name: "stream",
-      img: "/s.svg",
-      nameImg: "/streamName.svg",
-    },
-    {
-      id: 5,
-      name: "docker.",
-      img: "/dock.svg",
-      nameImg: "/dockerName.svg",
-    },
-  ];
-  
-  export const workExperience = [
-    {
-      id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
-      className: "md:col-span-2",
-      thumbnail: "/exp1.svg",
-    },
-    {
-      id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-      className: "md:col-span-2", // change to md:col-span-2
-      thumbnail: "/exp2.svg",
-    },
-    {
-      id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-      className: "md:col-span-2", // change to md:col-span-2
-      thumbnail: "/exp3.svg",
-    },
-    {
-      id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
-      className: "md:col-span-2",
-      thumbnail: "/exp4.svg",
-    },
-  ];
-  
-  export const socialMedia = [
-    {
-      id: 1,
-      img: "/git.svg",
-    },
-    {
-      id: 2,
-      img: "/twit.svg",
-    },
-    {
-      id: 3,
-      img: "/link.svg",
-    },
-  ];
+
+export const projects = [
+  {
+    id: 1,
+    title: "BuyBucket - E-commerce website",
+    des: "An ecommerce project built with the MERN stack (MongoDB, Express, React, Node.js) provides a full-featured platform for online shopping",
+    img: "/p1.png",
+    gitLink: "https://github.com/AaaSsHhUu/Ecommerce.git",
+    liveLink: "https://ecommerce-frontend-ashy-delta.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Airbnb Clone",
+    des: "An Airbnb clone built with the MERN stack (MongoDB, Express, React, Node.js) focuses on displaying rental properties with essential functionalities",
+    img: "/p2.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    gitLink: "https://github.com/AaaSsHhUu/airbnb-clone.git",
+    liveLink: "https://airbnb-clone-opal-gamma.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "GoFood - Food App",
+    des: "A food ordering app built with the MERN stack (MongoDB, Express, React, Node.js) focuses on showcasing a catalog of food items",
+    img: "/p3.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    gitLink: "https://github.com/AaaSsHhUu/Go-food-.git",
+    liveLink: "https://go-food-fc0g.onrender.com/",
+  },
+  // {
+  //   id: 4,
+  //   title: "Animated Apple Iphone 3D Website",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+  //   gitLink: "https://github.com/adrianhajdin/iphone",
+  //   liveLink: "#",
+  // },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Ashu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ashu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ashu is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Ashu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ashu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ashu is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Ashu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ashu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ashu is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Ashu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ashu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ashu is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Ashu was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ashu's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ashu is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - JSM Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
